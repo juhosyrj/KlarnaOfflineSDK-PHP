a528b9d6468812dc4eab099017b8f8907c12b96a
Mattias Nording <mattiasnording@gmail.com>
2016-10-05 16:22:51 +0200

    initial commit

---
+++ src/Entities/CartRow.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mattias.nording
+ * Date: 2016-10-05
+ * Time: 16:15
+ */
+
+namespace Klarna\Entities;
+
+
+class CartRow
+{
+
+}
---
+++ src/Entities/MerchantConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mattias.nording
+ * Date: 2016-10-05
+ * Time: 16:14
+ */
+
+namespace Klarna\Entities;
+
+
+class MerchantConfig
+{
+
+}
