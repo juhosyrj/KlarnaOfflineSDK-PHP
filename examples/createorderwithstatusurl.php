96d74be9d3222ce9bd19f4e7317776b121c05537
Mattias Nording <mattiasnording@gmail.com>
2016-10-07 14:45:20 +0200

    Updating example

--- examples/createorderwithstatusurl.php
+++ examples/createorderwithstatusurl.php
@@ -19,7 +19,11 @@ $item->reference="test22";
 $cart->AddProduct($item);
 
 $config = new MerchantConfig("Merchant_ID","Shared_SECRET","SEK","SE","sv-se",ServerMode::TEST);
-$t = new KlarnaSMSOrder($config,$cart,"PHONE","Terminal","Reference","https://orderInfoEndpoint.com");
+$t = new KlarnaSMSOrder($config,$cart,"PHONE","Terminal","Reference","https://myownsite.com/createorderwithstatusurl.php");
 $t->Create();
 //Order information will now be pushed to https://orderInfoEndpoint.com with raw post data
-
+if(isset($_POST))
+{
+    $postdata = file_get_contents("php://input");
+    var_dump($postdata);
+}
